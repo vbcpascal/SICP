8e1be4319767515da06fd906750b9be5d30753a6
vbcpascal <33946372+vbcpascal@users.noreply.github.com>
2018-06-26 15:13:35 +0800

    Create 26-mysum.rkt

---
+++ homework/26-mysum.rkt
@@ -0,0 +1,40 @@
+;http://lisp.test.openjudge.org/
+;26-mysum
+;vbcpascal
+
+#lang racket
+(define (mysum x)
+
+;;code begins here
+  (define (f . l)
+    (if (null? l)
+        x
+        (mysum (+ x (car l)))))
+  f)
+;;code ends here
+
+(define (f s n)
+  (define (exec g i) ; call g for i times
+    (if (= i 1)
+        (g)
+        (begin (g) (exec g (- i 1)))))
+    
+  (define (iter k ls)
+    (if (null? ls)
+        (exec k n) ;call k for n times. same effect as (k). just to prevent cheating
+        (iter (k (car ls)) (cdr ls))))
+  (iter (mysum (car s)) (cdr s)))
+
+(define k ((((mysum 1) 2) 3) 4))
+(define k2 ((mysum 10) 20))
+
+(displayln (k))
+(displayln (k2))
+
+(define (myloop)
+  (let ((s (read)))
+    (if (eq? s eof)
+        (void)
+        (begin (displayln (f s (car s)))
+               (myloop)))))
+(myloop)
